a46df12e41336883ca58508f59365de0a0edb35b
Lê Huỳnh Đức <76842196+lehuynhduc16@users.noreply.github.com>
2023-03-12 11:11:18 +0700

    Create Convert_A_Hex_String_To_RGB.py

---
+++ Convert_A_Hex_String_To_RGB.py
@@ -0,0 +1,17 @@
+"""
+When working with color values it can sometimes be useful to extract the individual red, green, and blue (RGB) component values for a color.
+Implement a function that meets these requirements:
+Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
+Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
+
+Example
+      "#FF9933" --> {r: 255, g: 153, b: 51}
+"""
+
+def hex_string_to_RGB(hex_string): 
+    r = int(hex_string[1:3], 16)
+    g = int(hex_string[3:5], 16)
+    b = int(hex_string[5:], 16)
+    p = {'r': r, 'g': g, 'b': b}
+    return p
