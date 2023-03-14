b2f17a762ef29bde5135334d8be0a24144cc00db
Lê Huỳnh Đức <76842196+lehuynhduc16@users.noreply.github.com>
2023-03-14 10:53:16 +0700

    Create STACK_SIM.py

---
+++ STACK_SIM.py
@@ -0,0 +1,42 @@
+"""
+Perform a sequence of operations over a stack, each element is an integer:
+PUSH v: push a value v into the stack 
+POP: remove an element out of the stack and print this element to stdout (print NULL if the stack is empty) 
+Input:
+      Each line contains a command (operration) of type:
+        + PUSH v
+        + POP 
+Output:
+      Write the results of POP operations (each result is written in a line)
+
+Example 
+Input:
+      PUSH 1 
+      PUSH 2 
+      PUSH 3 
+      POP 
+      POP 
+      PUSH 4 
+      PUSH 5 
+      POP 
+      # 
+Output:
+      3 
+      2 
+      5
+"""
+
+inp = "."
+p = []
+while(inp != "#"):
+    inp = input()
+    if inp=="#":
+        break
+    if "PUSH" in inp:
+        p.append(inp[5:])
+    else:
+        if len(p) > 0:
+            print(p[len(p)-1])
+            p.remove(p[len(p)-1])
+        else:
+            print("NULL")
