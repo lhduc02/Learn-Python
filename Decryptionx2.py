518f102b8ecf86ffe22ba82d1f82d08f9c391209
Lê Huỳnh Đức <76842196+lehuynhduc16@users.noreply.github.com>
2023-11-30 19:31:13 +0700

    Create Decryptionx2.py

---
+++ Decryptionx2.py
@@ -0,0 +1,51 @@
+Bạn được cung cấp một chuỗi x được tạo bởi các chữ số và kí tự 
+′
+.
+′
+′
+ . 
+′
+ . Chúng tôi muốn ánh xạ x sang các ký tự chữ thường tiếng Anh như sau: Các ký tự ( ‘a’ đến ‘i’ ) được biểu thị bằng ( ‘1’ đến ‘9’ ) tương ứng; Các ký tự ( ‘j’ đến ‘z’ ) được biểu thị bằng ( ‘10.’ đến ‘26.’ ) tương ứng.
+
+Yêu cầu: Hãy dựa vào quy tắc ở trên để trả về chuỗi được hình thành sau khi ánh xạ. .
+
+Input
+Một chuỗi x được tạo bởi các chữ số và kí tự 
+′
+.
+′
+′
+ . 
+′
+ 
+Constraints
+1
+≤
+�
+.
+�
+�
+�
+�
+�
+ℎ
+≤
+1000
+1≤x.length≤1000
+x consists of digits and the 
+′
+.
+′
+′
+ . 
+′
+  letter.
+x will be a valid string such that mapping is always possible.
+Output
+Trả về chuỗi được hình thành sau khi ánh xạ
+Example
+Input:
+1111111111111111111126.25.24.23.22.21.22.22.2223
+
+Output:
+aaaaaaaaaaaaaaaaaaaazyxwvuvvbbbc
